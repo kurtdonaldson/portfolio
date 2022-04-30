e0f2210e3dfcb1deb7c4a8e5ba787cefd80865ed
Kurt Donaldson <kurt_donaldson@hotmail.com>
2022-04-30 23:19:56 +1200

    index changes

--- apps.js
+++ apps.js
@@ -2,22 +2,36 @@ const dropMenu = document.querySelector("#slide-ul");
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
 dropMenu.style.marginTop = "-360px";
-dropMenu.style.transition = "0.3s"
+dropMenu.style.transition = "0.3s";
 
-
-function openMenu(){
-    dropMenu.style.marginTop = "20px";
+function openMenu() {
+  dropMenu.style.marginTop = "20px";
 }
- 
-function closeMenu(){
-    dropMenu.style.marginTop = "-360px";
+
+function closeMenu() {
+  dropMenu.style.marginTop = "-360px";
 }
 
 menuButton.addEventListener("click", () => {
-        if(dropMenu.style.marginTop == "-360px"){
-            openMenu()
-        } else {
-            closeMenu()
-        }
-    })
+  if (dropMenu.style.marginTop == "-360px") {
+    openMenu();
+  } else {
+    closeMenu();
+  }
+});
 
+const scriptURL = "https://getform.io/f/eca318f0-9de0-46d2-933a-1f70fb5eb1ca";
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
+    .then((response) => {
+      form.reset();
+      alert("Form sent!");
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
